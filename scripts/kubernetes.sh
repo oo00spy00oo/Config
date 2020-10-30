e8c2e1d4c21faa7910e29bd7e5a61b596d209bb5
Spy Kab <ooo000spy000ooo@gmail.com>
2020-10-30 23:50:27 +0700

    Add kubernetes plugins

---
+++ scripts/kubernetes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Plugins
+
+kubectl krew install apparmor-manager
+kubectl krew install ca-cert
+kubectl krew install cert-manager
+kubectl krew install debug
+kubectl krew install example
