db4e34c481b84f67b8d2e975300606711ce1f672
Spy Kab <ooo000spy000ooo@gmail.com>
2020-11-01 01:58:05 +0700

    Fixing calling shell function

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,24 +6,30 @@ brew upgrade
 brew upgrade --cask
 brew cleanup
 
-### SDK Man
+# ### SDK Man
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk update
 sdk upgrade
 
-### NeoVim
+# ### Prezto
+# Prezto
+source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+zprezto-update
+
+# ### NeoVim
 nvim --headless +PlugUpdate +qall
 nvim --headless +PlugUpgrade +qall
 
-### Prezto
-zprezto-update
-
-### zinit
+# ### zinit
+source "$HOME/.zinit/bin/zinit.zsh"
 # To update Zinit issue zinit self-update in the command line.
 zinit self-update
 # To update all plugins and snippets, issue zinit update
 zinit update
 
 # nvm upgrade
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm upgrade
 
 # Update .oh-my-tmux
