450b243603edc82d13fab02b028ce0c25ac36990
Spy Kab <ooo000spy000ooo@gmail.com>
2020-11-01 01:20:37 +0700

    Add zinit update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Homebrew
+### Homebrew
 brew update
 brew upgrade
 brew upgrade --cask
 brew cleanup
 
-# SDK Man
+### SDK Man
 sdk update
 sdk upgrade
 
-# Prezto
+### Prezto
 zprezto-update
+
+### zinit
+# To update Zinit issue zinit self-update in the command line.
+zinit self-update
+# To update all plugins and snippets, issue zinit update
+zinit update
 
 # nvm upgrade
 nvm upgrade
