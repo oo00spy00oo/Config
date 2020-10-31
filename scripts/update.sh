5bae1b922064117d51ccebe637a307be8452aa9c
Spy Kab <ooo000spy000ooo@gmail.com>
2020-11-01 01:59:52 +0700

    Add logging

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 ### Homebrew
+echo "Updating Homebrew..."
 brew update
 brew upgrade
 brew upgrade --cask
 brew cleanup
 
+# ### NeoVim
+echo "Updating NeoVim..."
+nvim --headless +PlugUpdate +qall
+nvim --headless +PlugUpgrade +qall
+
 # ### SDK Man
+echo "Updating SDK Man..."
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk update
 sdk upgrade
 
 # ### Prezto
 # Prezto
+echo "Updating Prezto..."
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 zprezto-update
 
-# ### NeoVim
-nvim --headless +PlugUpdate +qall
-nvim --headless +PlugUpgrade +qall
-
 # ### zinit
+echo "Updating zinit..."
 source "$HOME/.zinit/bin/zinit.zsh"
 # To update Zinit issue zinit self-update in the command line.
 zinit self-update
@@ -28,9 +33,13 @@ zinit self-update
 zinit update
 
 # nvm upgrade
+echo "Updating nvm..."
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm upgrade
 
 # Update .oh-my-tmux
+echo "Updating oh-my-tmux..."
 cd $HOME/Config && git submodule update
+
+echo "Finishing update!"
