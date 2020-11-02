be9c39893fe234c1f9dbf7d71bc29d283b3c7c87
Spy Kab <ooo000spy000ooo@gmail.com>
2020-11-03 00:35:47 +0700

    Finish Homebrew packages

--- scripts/install/libs/homebrew.py
+++ scripts/install/libs/homebrew.py
@@ -9,16 +9,62 @@ console = Console()
 class Homebrew:
 
     def exec_single(self, file_name, is_cask=False):
-        if is_cask:
-            utils.exec_single(command.BREW_CASK_CMD, file_name)
-        else:
-            utils.exec_single(command.BREW_CMD, file_name)
+        stream = utils.load_yaml(file_name)
+        if stream is None:
+            console.print("No packages found in [bold red]" + file_name + "[/]" + "!")
+            return
+        for app in stream:
+            print()
+            console.print("Installing [bold red]" + app.get("name") + "[/]...")
+            package = app.get(command.CODE)
+            tap = app.get(command.TAP)
+            depends = app.get(command.DEPENDS)
+
+            if tap is not None:
+                self.tap(tap)
+            if depends is not None:
+                self.handle_depends(depends)
+
+            if is_cask:
+                utils.exec_single(command.BREW_CASK_CMD, package)
+            else:
+                utils.exec_single(command.BREW_CMD, package)
 
     def exec_multiple(self, file_name, is_cask=False):
-        if is_cask:
-            utils.exec_multiple(command.BREW_CASK_CMD, file_name)
-        else:
-            utils.exec_multiple(command.BREW_CMD, file_name)
+        oneline = ""
+        stream = utils.load_yaml(file_name)
+        if stream is None:
+            console.print("No packages found in [bold red]" + file_name + "[/]" + "!")
+            return
+        for app in stream:
+            package = app.get(command.CODE)
+            tap = app.get(command.TAP)
+            depends = app.get(command.DEPENDS)
 
-    def tap(self, tap):
-        utils.shell_command(command.BREW_TAP_CMD, tap)
+            if tap is not None:
+                self.tap(tap)
+            if depends is not None:
+                self.handle_depends(depends)
+
+            oneline += " " + package
+
+        if is_cask:
+            utils.exec_multiple(command.BREW_CASK_CMD, oneline)
+        else:
+            utils.exec_multiple(command.BREW_CMD, oneline)
+
+    def handle_depends(self, depends):
+        for depend in depends:
+            if depend.get(command.FOMULAE) is not None:
+                self.formulae(depend.get(command.FOMULAE))
+            if depend.get(command.CASK) is not None:
+                self.cask(depend.get(command.CASK))
+
+    def tap(self, package):
+        utils.exec_single(command.BREW_TAP_CMD, package)
+
+    def formulae(self, package):
+        utils.exec_single(command.BREW_CMD, package)
+
+    def cask(self, package):
+        utils.exec_single(command.BREW_CASK_CMD, package)
--- scripts/install/main.py
+++ scripts/install/main.py
@@ -13,33 +13,81 @@ with open("README.md") as readme:
     markdown = Markdown(readme.read())
 console.print(markdown)
 
-console.print("Installing [bold red]CLI Tools[/] from [bold magenta]Homebrew[/]...")
-homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.CLI_FILE)
-homebrew.exec_multiple(location.HOMEBREW_CASK + location.CLI_FILE, True)
+console.rule("Install package from?")
+console.print("0. [magenta]All[/]")
+console.print("1. [magenta]Homebrew[/]")
+console.print("2. [magenta]SDK Man[/]")
+console.print("3. [magenta]pip[/]")
+console.print("4. [magenta]node[/]")
+console.print("4. [magenta]Kubernetes Plugins[/]")
 
-console.print("Installing [bold red]DevOps Tools[/] from [bold magenta]Homebrew[/]...")
-homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.DEVOPS_TOOLS_FILE)
-homebrew.exec_multiple(location.HOMEBREW_CASK + location.DEVOPS_TOOLS_FILE, True)
+prefix_cmd = "cd $HOME/Config/scripts/install && "
 
-console.print("Installing [bold red]DevOps Tools[/] from [bold magenta]Homebrew[/]...")
-homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.DEVOPS_TOOLS_FILE)
-homebrew.exec_multiple(location.HOMEBREW_CASK + location.DEVOPS_TOOLS_FILE, True)
+def install_homebrew():
+    console.print("Installing [bold red]CLI Tools[/] from [bold magenta]Homebrew[/]...")
+    homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.CLI_FILE)
+    homebrew.exec_multiple(location.HOMEBREW_CASK + location.CLI_FILE, True)
 
-console.print("Installing [bold red]Desktop Application[/] from [bold magenta]Homebrew[/]...")
-homebrew.exec_single(location.HOMEBREW_FORMULAE + location.DESKTOP_APPICATION_FILE)
-homebrew.exec_single(location.HOMEBREW_CASK + location.DESKTOP_APPICATION_FILE, True)
+    console.print("Installing [bold red]DevOps Tools[/] from [bold magenta]Homebrew[/]...")
+    homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.DEVOPS_TOOLS_FILE)
+    homebrew.exec_multiple(location.HOMEBREW_CASK + location.DEVOPS_TOOLS_FILE, True)
 
-console.print("Installing [bold red]Font[/] from [bold magenta]Homebrew[/]...")
-homebrew.tap("homebrew/cask-fonts")
-homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.FONT_FILE)
-homebrew.exec_multiple(location.HOMEBREW_CASK + location.FONT_FILE, True)
+    console.print("Installing [bold red]DevOps Tools[/] from [bold magenta]Homebrew[/]...")
+    homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.DEVOPS_TOOLS_FILE)
+    homebrew.exec_multiple(location.HOMEBREW_CASK + location.DEVOPS_TOOLS_FILE, True)
 
-def install_others():
-    # Git toolbelt
-    console.print("Installing [bold red]Git toolbelt[/]...")
-    os.system("brew tap nvie/tap")
-    os.system("brew install nvie/tap/git-toolbelt")
-    # Telepresence
-    console.print("Installing [bold red]Telepresence[/]...")
-    os.system("brew cask install osxfuse")
-    os.system("brew install datawire/blackbird/telepresence")
+    console.print("Installing [bold red]Desktop Application[/] from [bold magenta]Homebrew[/]...")
+    homebrew.exec_single(location.HOMEBREW_FORMULAE + location.DESKTOP_APPICATION_FILE)
+    homebrew.exec_single(location.HOMEBREW_CASK + location.DESKTOP_APPICATION_FILE, True)
+
+    console.print("Installing [bold red]Font[/] from [bold magenta]Homebrew[/]...")
+    homebrew.tap("homebrew/cask-fonts")
+    homebrew.exec_multiple(location.HOMEBREW_FORMULAE + location.FONT_FILE)
+    homebrew.exec_multiple(location.HOMEBREW_CASK + location.FONT_FILE, True)
+
+def install_sdkman():
+    console.print()
+    console.print("Installing [bold red]SDK Man[/]...")
+    cmd = prefix_cmd + "sdkman.sh"
+    os.system(cmd)
+    # sdkman_thread = subprocess.Popen(["sh", "install/sdkman.sh"])
+    # sdkman_thread.wait()
+
+def install_pip():
+    console.print()
+    console.print("Installing [bold red]pip[/] packages...")
+    cmd = prefix_cmd + "pip.sh"
+    os.system(cmd)
+
+def install_node():
+    console.print()
+    console.print("Installing [bold red]Node[/] packages...")
+    cmd = prefix_cmd + "node.sh"
+    os.system(cmd)
+
+def install_kubernetes_plugins():
+    console.print()
+    console.print("Installing [bold red]Kubernetes Plugins[/]...")
+    cmd = prefix_cmd + "kubernetes.sh"
+    os.system(cmd)
+
+choice = console.input("What is your [bold red]choice[/]? :smiley: ")
+console.print(f'You entered {choice}')
+choice = int(choice)
+
+if choice == 0:
+    install_homebrew()
+    install_sdkman()
+    install_pip()
+    install_node()
+    install_kubernetes_plugins()
+elif choice == 1:
+    install_homebrew()
+elif choice == 2:
+    install_sdkman()
+elif choice == 3:
+    install_pip()
+elif choice == 4:
+    install_node()
+elif choice == 5:
+    install_kubernetes_plugins()
