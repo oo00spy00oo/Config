de0172cae4d7a32b7e4ed4f52525a89e87b15bf7
Spy Kab <ooo000spy000ooo@gmail.com>
2020-11-06 17:51:11 +0700

    :art: Add plugins

--- scripts/update/zinit.sh
+++ scripts/update/zinit.sh
@@ -1,7 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
-source "$HOME/.zinit/bin/zinit.zsh"
+# source "$HOME/.zinit/bin/zinit.zsh"
+echo $(which zinit)
+# bat $HOME/.zinit/bin/zinit.zsh
+# which zinit
 # To update Zinit issue zinit self-update in the command line.
-zinit self-update
+# zinit self-update
 # To update all plugins and snippets, issue zinit update
-zinit update
+# zinit update
